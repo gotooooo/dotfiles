68d7a6cc47507a103ebcb131cb8e4ab75cde00cf
gotooooo <k.goto.123@gmail.com>
2021-01-24 18:26:01 +0900

    use zinit

--- update_zshrc.sh
+++ update_zshrc.sh
@@ -1,4 +1,8 @@
 #!/bin/zsh
 
-\cp -f $HOME/dotfiles/.zshrc $HOME/.zshrc
+echo "zinit snippet $HOME/dotfiles/.zshrc.path" >> "$HOME/.zshrc"
+echo "zinit snippet $HOME/dotfiles/.zshrc.base" >> "$HOME/.zshrc"
+echo "zinit snippet $HOME/dotfiles/.zshrc.alias" >> "$HOME/.zshrc"
+echo "zinit snippet $HOME/dotfiles/.zshrc.opt" >> "$HOME/.zshrc"
+echo "zinit snippet $HOME/.zshrc.local" >> "$HOME/.zshrc"
 printf '\033[32m%s\033[m\n' '.zshrc is updated'
